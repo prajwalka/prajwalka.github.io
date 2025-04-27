10138de95c816548b5de34c46b7a5c5342807ea7
Prajwal.K.A <prajwalka@MacBook-Air-5.local>
2025-04-27 19:11:51 -0400

    Updating homepage

--- assets/script.js
+++ assets/script.js
@@ -4,10 +4,10 @@ const header = document.getElementById('header');
 // Listen for the scroll event
 window.onscroll = function() {
   if (window.scrollY > 2) { // Change this value as per your preference
-    header.style.backgroundColor = "#002D72"; // Heritage Blue
-    header.style.color = "white"; // Text becomes white
+    header.style.backgroundColor = "black"; // Heritage Blue
+    header.style.color = "white";
   } else {
     header.style.backgroundColor = "white"; // Default background
-    header.style.color = "#002D72"; // Heritage Blue text
+    header.style.color = "black"; 
   }
 };
